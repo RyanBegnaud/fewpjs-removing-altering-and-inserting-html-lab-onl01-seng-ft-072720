1f3c690f025fb7cb002f228ce9b21190193bf31e
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-11-24 23:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector("main#main").remove();
 // let newHeader = document.getElementById("h1#victory")
 let newHeader = document.createElement('h1')
-newHeader.innerHTML = "victory";
+newHeader.id = "victory";
