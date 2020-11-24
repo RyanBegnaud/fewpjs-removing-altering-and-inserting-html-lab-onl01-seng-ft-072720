3d465784c88ec1740b17ba23b1fcf543b5d4410a
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-11-24 23:07:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.querySelector("main#main").remove();
 let newHeader = document.getElementById("h1#victory");
+newHeader.innerHTML = `<h1>Victory</h1>
 newHeader.innerHTML = "YOUR-NAME is the champion";
